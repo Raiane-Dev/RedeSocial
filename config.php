368018ee0e6db63a4507952128bade62f8f82ee7
Raiane Dev <89032013+Raiane-Dev@users.noreply.github.com>
2021-11-07 19:30:13 -0300

    Update config.php

--- config.php
+++ config.php
@@ -12,7 +12,7 @@
 	spl_autoload_register($autoload);
 
 
-	define('INCLUDE_PATH','http://localhost/Curso/Projeto/Rede%20Social/');
+	define('INCLUDE_PATH','http://localhost/Curso/Projeto/RedeSocial/');
 	define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
 
 	define('BASE_DIR_PAINEL',__DIR__.'/painel');
